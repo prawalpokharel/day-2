f9bcdc12b7a2f565881d01bfccb8fcc881053b86
prawal Pokharel <prawalpokharel@prawals-mbp.lan>
2021-12-30 14:19:19 -0500

    Initial Commit

---
+++ src/day3/FunctionConcept.java
@@ -0,0 +1,15 @@
+package day3;
+
+public class FunctionConcept {
+
+    public static void main(String[] args){
+        student ram = new student();
+        ram.name = "Ram";
+        ram.playing();
+        student rita = new student();
+        rita.name = "rita";
+        rita.studying();
+
+    }
+
+}
---
+++ src/day3/student.java
@@ -0,0 +1,13 @@
+package day3;
+
+public class student {
+    static String name;
+
+    public void playing() {
+        System.out.println(student.name + " is playing...");
+    }
+    public void studying() {
+        System.out.println(student.name + " is studying...");
+    }
+}
+
